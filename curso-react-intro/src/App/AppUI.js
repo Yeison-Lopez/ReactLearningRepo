b52c7f295aec9606be417c1456eebe04bc7aab07
YeisonGit <bogotavr@gmail.com>
2026-06-11 21:14:12 -0500

    changes made until class 20

--- curso-react-intro/src/App/AppUI.js
+++ curso-react-intro/src/App/AppUI.js
@@ -6,6 +6,8 @@ import { CreateTodoButton } from '../CreateTodoButton';
 
 
 function AppUI({
+    loading,
+    error,
     completedTodos,
     totalTodos,
     searchValue,
@@ -25,6 +27,11 @@ function AppUI({
         />
 
         <TODOlist>
+            {loading && <p>estamos cargando...</p>}
+            {error && <p>Desespérate, hubo un error</p>}
+            {(!loading && searchedTodos.length===0) && <p>Crea tu primer TODO!</p>}
+
+
             {searchedTodos.map(todo =>(
             <TodoItem
                 key={todo.text}
--- curso-react-intro/src/App/index.js
+++ curso-react-intro/src/App/index.js
@@ -5,20 +5,15 @@ import { useLocalStorage } from './useLocalStorage';
 
 function App() {
  
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1',[]); 
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,} = useLocalStorage('TODOS_V1',[]); 
   const [searchValue, setSearchValue] = React.useState(''); 
 
   const completedTodos = todos.filter(todo => todo.completed).length;
   const totalTodos = todos.length;
-
-  console.log('Log 1');
-
-  console.log('Log 2');
-
-  console.log('Log 3');
-
-
-
 
 
   const searchedTodos = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()));
@@ -43,6 +38,8 @@ function App() {
 
   return(
     <AppUI
+      loading={loading}
+      error={error}
       completedTodos={completedTodos}
       totalTodos={totalTodos}
       searchValue={searchValue}
